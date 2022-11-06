0bddfff76d81188a8f8a9898fd163a719e9e302c
Jan Malej <jan.malej12@gmail.com>
2022-11-06 15:27:45 +0100

    html spletne strani zapišemo v datoteko

--- restavracije_zajem.py
+++ restavracije_zajem.py
@@ -1,4 +1,41 @@
-# najprej je treba html spletne strani zapisati v lokalen txt file
+import orodja
+import re
+import os
+import requests
+
+IME_DATOTEKE = 'raw_html.txt' 
+PATH = 'Datoteke'
+URL ='https://www.tripadvisor.com/RestaurantSearch-g35805-oa00-Chicago_Illinois.html#EATERY_LIST_CONTENTS'
+
+def spl_stran_v_string(url):
+    try:
+        r = requests.get(url)
+    except requests.exceptions.ConnectionError:  
+        print('Ni povezave.')
+        return None
+    if r.status_code == requests.codes.ok:
+        return r.text
+    else:
+        print('Napaka.')
+        return None
+
+def string_to_datoteka(niz):
+    os.makedirs(PATH, exist_ok=True)
+    path = os.path.join(PATH, IME_DATOTEKE)
+    with open(path, 'w', encoding='utf-8') as file_out:
+        file_out.write(niz)
+    return None
+
+def shrani_stran():
+    niz = spl_stran_v_string(URL)
+    string_to_datoteka(niz)
+    return None
+
+shrani_stran()
+
+
+
+
 # nato z regexi ločimo na bloke - restavracije
 # nato z regexi prefiltriram vsak blok
 # dobljeno zapišemo v csv
